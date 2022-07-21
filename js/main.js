dca1d55b5a883f59e6e2aef9f024eac672682157
DaniDuese <74594229+DaniEnsi@users.noreply.github.com>
2022-07-21 12:51:25 +0200

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,9 +1,4 @@
-/**
-* Template Name: Personal - v4.7.0
-* Template URL: https://bootstrapmade.com/personal-free-resume-bootstrap-template/
-* Author: BootstrapMade.com
-* License: https://bootstrapmade.com/license/
-*/
+
 (function() {
   "use strict";
 
