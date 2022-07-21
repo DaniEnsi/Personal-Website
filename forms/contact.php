7844c58ec3ec5f55eb8f9099903a1a23f2e61cad
DaniDuese <74594229+DaniEnsi@users.noreply.github.com>
2022-07-21 12:48:06 +0200

    Update contact.php

--- forms/contact.php
+++ forms/contact.php
@@ -1,13 +1,10 @@
 <?php
   /**
   * Requires the "PHP Email Form" library
-  * The "PHP Email Form" library is available only in the pro version of the template
-  * The library should be uploaded to: vendor/php-email-form/php-email-form.php
-  * For more info and help: https://bootstrapmade.com/php-email-form/
   */
 
   // Replace contact@example.com with your real receiving email address
-  $receiving_email_address = 'daniel@ensminger.de';
+  $receiving_email_address = 'youremail';
 
   if( file_exists($php_email_form = '../assets/vendor/php-email-form/php-email-form.php' )) {
     include( $php_email_form );
